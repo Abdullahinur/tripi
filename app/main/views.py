a94e37de338699f21c26982a470d6f79fbc7174a
Abdullahinur <aabdullahinur@hotmail.com>
2018-04-25 14:28:38 +0300

    add blue css style

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,10 @@
-from flask import render_template,request,redirect,url_for
+from flask import render_template, request, redirect, url_for
 from . import main
 
 # Views
+
+
 @main.route('/')
 def index():
-	title = 'Flask Base'
-	return render_template('index.html',title = title)
+    title = 'Flask Base'
+    return render_template('index.html', title=title)
